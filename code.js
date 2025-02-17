9e534ceb33a6c4703d8bb7805ea606d962f265ed
mrvampire-cybr <iqooz4695@gmail.com>
2025-02-17 16:03:39 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+/* animation_trigger.js */
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the Shiva icon element
+  var shivaIcon = document.querySelector(".shiva-icon");
+
+  // Add the 'glowing' class to trigger the glow effect
+  if (shivaIcon) {
+    shivaIcon.classList.add("glowing");
+  }
+});
